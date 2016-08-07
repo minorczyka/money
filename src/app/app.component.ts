71a04a0830508290a7313ba65eefd826e02491ca
kompu <kompu.pliczki@gmail.com>
2016-08-07 13:42:05 +0200

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
+@Component({
+  selector: 'my-app',
+  providers: [ApiService],
+  directives: [...ROUTER_DIRECTIVES],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  url = 'https://github.com/preboot/angular2-webpack';
+
+  constructor(private api: ApiService) {
+  }
+}
